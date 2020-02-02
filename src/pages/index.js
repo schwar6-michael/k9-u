9adeb07b2d2c0c9317077e8bf1a5374a5cbc0c7f
Michael <michael@correlation-one.com>
2020-02-02 14:20:27 -0500

    latest updates for form

--- src/pages/index.js
+++ src/pages/index.js
@@ -55,6 +55,28 @@ const AppWrapper = styled.div`
   z-index: 0;
 `
 
+const Name = styled.span`
+  font-family: "Roboto", sans-serif;
+  color: #000000;
+  font-weight: 300;
+  line-height: 30px;
+  font-size: 1.4em;
+  text-align: center;
+  padding-top: 3px;
+`
+
+const Success = styled.p`
+  font-family: "Roboto", sans-serif;
+  color: #53924f;
+  font-weight: 600;
+  line-height: 30px;
+  font-size: 1em;
+  text-align: center;
+  padding-top: 3px;
+  margin-bottom: 20px;
+  text-transform: uppercase;
+`
+
 const Modal = styled.div`
   position: fixed;
   top: 0;
@@ -67,6 +89,20 @@ const Modal = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  .successMessage {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    align-items: center;
+  }
+  .successModal {
+    display: flex;
+    align-items: center;
+    .loader {
+      margin-right: 20px;
+      animation: spin 1.5s infinite linear;
+    }
+  }
   form {
     position: relative;
     width: 500px;
@@ -79,7 +115,7 @@ const Modal = styled.div`
     padding: 40px 7%;
     cursor: pointer;
     animation: fadeInDown 400ms ease-out;
-    svg {
+    .exit {
       position: absolute;
       top: -30px;
       right: -30px;
@@ -88,6 +124,14 @@ const Modal = styled.div`
     }
     svg:hover {
       transform: scale(1.2);
+    }
+  }
+  @keyframes spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
     }
   }
   @keyframes fadeInDown {
@@ -156,6 +200,8 @@ function encode(data) {
 const IndexPage = () => {
   const [modal, setModal] = useState(false)
   const [state, setState] = useState()
+  const [success, setSuccess] = useState(false)
+  const [successLoading, setSuccessLoading] = useState(false)
   const Section2 = useRef(null)
   const Section3 = useRef(null)
   const Section4 = useRef(null)
@@ -169,6 +215,7 @@ const IndexPage = () => {
 
   const handleSubmit = e => {
     e.preventDefault()
+    setSuccessLoading(true)
     const form = e.target
     fetch("/", {
       method: "POST",
@@ -178,7 +225,11 @@ const IndexPage = () => {
         ...state,
       }),
     })
-      .then(() => navigate(form.getAttribute("action")))
+      .then(() => {
+        navigate(form.getAttribute("action"))
+        setSuccessLoading(false)
+        setSuccess(true)
+      })
       .catch(error => alert(error))
   }
 
@@ -187,6 +238,7 @@ const IndexPage = () => {
   }
 
   const handleOpenModal = () => {
+    setSuccess(false)
     setModal(true)
   }
 
@@ -201,31 +253,80 @@ const IndexPage = () => {
             data-netlify-honeypot="bot-field"
             onSubmit={handleSubmit}
           >
-            <svg
-              onClick={handleCloseModal}
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-            >
-              <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.151 17.943l-4.143-4.102-4.117 4.159-1.833-1.833 4.104-4.157-4.162-4.119 1.833-1.833 4.155 4.102 4.106-4.16 1.849 1.849-4.1 4.141 4.157 4.104-1.849 1.849z" />
-            </svg>
-            <input type="hidden" name="form-name" value="contact" />
-            <label>
-              Full Name:{" "}
-              <input type="text" name="name" onChange={handleChange} />
-            </label>
-            <label>
-              Email: <input type="email" name="email" onChange={handleChange} />
-            </label>
-            <label>
-              Number: <input type="tel" name="number" onChange={handleChange} />
-            </label>
-            <label>
-              Message:
-              <textarea name="message" onChange={handleChange} />{" "}
-            </label>
-            <input type="submit" />
+            {successLoading ? (
+              <div className="successModal">
+                <svg
+                  className="exit"
+                  onClick={handleCloseModal}
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.151 17.943l-4.143-4.102-4.117 4.159-1.833-1.833 4.104-4.157-4.162-4.119 1.833-1.833 4.155 4.102 4.106-4.16 1.849 1.849-4.1 4.141 4.157 4.104-1.849 1.849z" />
+                </svg>
+                <svg
+                  className="loader"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="#53924f"
+                >
+                  <path d="M13.75 22c0 .966-.783 1.75-1.75 1.75s-1.75-.784-1.75-1.75.783-1.75 1.75-1.75 1.75.784 1.75 1.75zm-1.75-22c-1.104 0-2 .896-2 2s.896 2 2 2 2-.896 2-2-.896-2-2-2zm10 10.75c.689 0 1.249.561 1.249 1.25 0 .69-.56 1.25-1.249 1.25-.69 0-1.249-.559-1.249-1.25 0-.689.559-1.25 1.249-1.25zm-22 1.25c0 1.105.896 2 2 2s2-.895 2-2c0-1.104-.896-2-2-2s-2 .896-2 2zm19-8c.551 0 1 .449 1 1 0 .553-.449 1.002-1 1-.551 0-1-.447-1-.998 0-.553.449-1.002 1-1.002zm0 13.5c.828 0 1.5.672 1.5 1.5s-.672 1.501-1.502 1.5c-.826 0-1.498-.671-1.498-1.499 0-.829.672-1.501 1.5-1.501zm-14-14.5c1.104 0 2 .896 2 2s-.896 2-2.001 2c-1.103 0-1.999-.895-1.999-2s.896-2 2-2zm0 14c1.104 0 2 .896 2 2s-.896 2-2.001 2c-1.103 0-1.999-.895-1.999-2s.896-2 2-2z" />
+                </svg>
+                <Name>Submitting...</Name>
+              </div>
+            ) : success ? (
+              <div className="successMessage">
+                <svg
+                  className="exit"
+                  onClick={handleCloseModal}
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.151 17.943l-4.143-4.102-4.117 4.159-1.833-1.833 4.104-4.157-4.162-4.119 1.833-1.833 4.155 4.102 4.106-4.16 1.849 1.849-4.1 4.141 4.157 4.104-1.849 1.849z" />
+                </svg>
+                <Success>Submission Successful!</Success>
+                <Name>
+                  Thank you for your interest in K9-University! We will be
+                  getting back to you as soon as possible!
+                </Name>
+              </div>
+            ) : (
+              <>
+                <svg
+                  className="exit"
+                  onClick={handleCloseModal}
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                >
+                  <path d="M12 0c-6.627 0-12 5.373-12 12s5.373 12 12 12 12-5.373 12-12-5.373-12-12-12zm4.151 17.943l-4.143-4.102-4.117 4.159-1.833-1.833 4.104-4.157-4.162-4.119 1.833-1.833 4.155 4.102 4.106-4.16 1.849 1.849-4.1 4.141 4.157 4.104-1.849 1.849z" />
+                </svg>
+                <input type="hidden" name="form-name" value="contact" />
+                <label>
+                  Full Name:{" "}
+                  <input type="text" name="name" onChange={handleChange} />
+                </label>
+                <label>
+                  Email:{" "}
+                  <input type="email" name="email" onChange={handleChange} />
+                </label>
+                <label>
+                  Number:{" "}
+                  <input type="tel" name="number" onChange={handleChange} />
+                </label>
+                <label>
+                  Message:
+                  <textarea name="message" onChange={handleChange} />{" "}
+                </label>
+                <input type="submit" />
+              </>
+            )}
           </form>
         </Modal>
       )}
